985f8f49db844c50dff989cd871a5586864c8dc0
ysm-suzuki <ys443ys443@yahoo.co.jp>
2017-10-26 14:22:46 +0900

    Initial source commit.

---
+++ cs/src/RepeatTimer.cs
@@ -0,0 +1,43 @@
+﻿public class RepeatTimer
+{
+    private float _elaspedTime = 0;
+    private float _limitTime = 0;
+    private bool _isPaused = false;
+
+    private System.Action _onTriggered;
+
+    public RepeatTimer(float limitTime, System.Action callback)
+    {
+        _limitTime = limitTime;
+        _onTriggered = callback;
+    }
+
+    public void Tick(float delta)
+    {
+        if (_isPaused)
+            return;
+
+        _elaspedTime += delta;
+        if (_elaspedTime >= _limitTime)
+        {
+            _elaspedTime = 0;
+            if (_onTriggered != null)
+                _onTriggered();
+        }
+    }
+
+    public void SetInterval(float limitTime)
+    {
+        _limitTime = limitTime;
+    }
+
+    public void Pause()
+    {
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        _isPaused = false;
+    }
+}
---
+++ cs/src/WaitTimer.cs
@@ -0,0 +1,24 @@
+﻿public class WaitTimer
+{
+    private float m_elaspedTime = 0;
+    private float m_limitTime = 0;
+
+    private System.Action m_onFinished;
+
+    public WaitTimer(float limitTime, System.Action callback)
+    {
+        m_limitTime = limitTime;
+        m_onFinished = callback;
+    }
+
+    public void Tick(float delta)
+    {
+        if (m_elaspedTime >= m_limitTime)
+            return;
+
+        m_elaspedTime += delta;
+        if (m_elaspedTime >= m_limitTime)
+            if (m_onFinished != null)
+                m_onFinished();
+    }
+}
